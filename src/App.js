bad0514c3d7eced2308e89467dc6aeb740fc58c3
sumitsharma-hub <sumit.sharma5099@gmail.com>
2021-02-12 15:40:50 +0530

    added some more rows

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import Nav from './Nav'
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Banner/>
-      <Row title="Netflix original" fetchUrl={requests.fetchNetflixOriginal} islargeRow/>
-      <Row title="Top trending" fetchUrl={requests.fetchTrending}/>
-      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies}/>
-       {/* <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="DocumentryMovies" fetchUrl={requests.fetchDocumentryMovies}/>
-      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies}/>   */}
+      <Nav />
+      <Banner />
+      <Row title="Netflix original" fetchUrl={requests.fetchNetflixOriginal} islargeRow />
+      <Row title="Top trending" fetchUrl={requests.fetchTrending} />
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="DocumentryMovies" fetchUrl={requests.fetchDocumentryMovies} />
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
